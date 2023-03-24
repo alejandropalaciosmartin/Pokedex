2b19c5d7eec373a98e4979dcdb4baf8557e86c91
Alejandro Palacios Martín <srjalean@gmail.com>
2023-03-24 21:43:27 +0100

    Update javascriptGeneral.js

--- javascriptGeneral.js
+++ javascriptGeneral.js
@@ -1,9 +1,10 @@
-const urlPokeApi = "https://pokeapi.co/api/v2/pokemon/"; //Para tener la dirección en una variable
-// const urlPokeApi = "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"; //Coger los siguientes 20 pokemons
+for(let j =1; j<=151;j++){
+
+const urlPokeApi = `https://pokeapi.co/api/v2/pokemon/?offset=${j}&limit=${j}`; //Coger los siguientes 20 pokemons
 const main = document.querySelector("main");//Seleccionamos la etiqueta de destino
 
 //Coge TODOS los pokemons que queremos NO HACE NADA...MEJORAR PARA MOSTRAR TODOS
-async function takeAll(url){ 
+async function take(url){ 
        // fetch(url)//Cogemos la dirección API para trabajar con ella
        // .then(resp => resp.json()) //La API la pasamos a json para poder trabajar con ella
         //.then(resp => console.log(resp)) //Vemos la información de la API en consola
@@ -14,17 +15,15 @@ async function takeAll(url){
         template = ""; //Donde concatenaremos luego el html para mostrar luego
 
 
-        //console.log(json); //APARECE los 20 PRIMEROS pokemons
-
         if(!res.ok) throw{status:res.status,statusText: statusText} //Si la respuesta de la API no (!) es ok(es la respuesta que da el
                                                                     //servidor), lanza error guardando en las variables status y 
                                                                     //statusText el estado para informar
 
-        for(let i = 0; i < json.results.length; i++){ //se usa for porque es bloqueante y podemos ir cogiendo y analizando cada paso, con 
+         //se usa for porque es bloqueante y podemos ir cogiendo y analizando cada paso, con 
                                                       //foreach lo coge del tirón tal como recibe
             //console.log(json.results[i]); //APARECE de CADA pokemon su NOMBRE y URL (donde contiene el resto de información), nada más
             try{
-                let res = await fetch(json.results[i].url), //cogemos de la api el json creado al principio, de ese json de la url
+                let res = await fetch(json.url), //cogemos de la api el json creado al principio, de ese json de la url
                                                             //especificamos que queremos coger el results que aparece en la API pero de cada
                                                             //iteracción/pokemon de la API con [i] y cogemos su dirección específica con .url
                                                             //que aparece también en la API
@@ -35,7 +34,7 @@ async function takeAll(url){
             if(!res.ok) throw{status:res.status,statusText: statusText}
 
             //Vamos llenando el html con cada interacción
-            template += ` 
+            template = ` 
             <div>
                 <imgsrc="${pokemon.sprites.front_default}></img>
                 <p>${pokemon.name}</p>
@@ -53,9 +52,9 @@ async function takeAll(url){
                 </figure>
                 `;
             }
-        }
-        main.innerHTML += template;
-        main.innerHTML += "xx";
+        
+        main.innerHTML = template;
+          
     }
     catch(err){
         console.log(err);
@@ -67,8 +66,9 @@ async function takeAll(url){
                                                                   //o de la API u otro mandado, según arriba manipulado
     }
 }
+}
 
-document.addEventListener("DOMContentLoaded",e=>takeAll(urlPokeApi)); //Donde se va a cargar todo
+document.addEventListener("DOMContentLoaded",e=>take(urlPokeApi)); //Donde se va a cargar todo
 
 
 
