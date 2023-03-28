7b0e12ade57ed451cce36cd7f5f18376eab510ab
adrian <adrianlm1985@gmail.com>
2023-03-28 11:03:42 +0200

    Nuevo

--- OtrasCosas/prueba apiPokemon/javascript.js
+++ OtrasCosas/prueba apiPokemon/javascript.js
@@ -1,5 +1,5 @@
 //const apiButton = document.getElementById('apiBoton'); //Seleccionamos el html y metemos variable para trabajar con él
-const apiData = document.getElementById('apiData');
+/*const apiData = document.getElementById('apiData');
 const baseExp = document.getElementById('baseExp');
 
 const callAPI = () => { //Creamos función que asignaremos al botón luego
@@ -15,5 +15,51 @@ const callAPI = () => { //Creamos función que asignaremos al botón luego
         .catch(e => console.error(new Error(e)));
 }
 
-callAPI();
+callAPI();*/
 //apiButton.addEventListener('click', callAPI); //Aplicamos al botón la función al hacer click
+
+let container = document.querySelector(".pokemons");
+
+const urlPokeApi = "https://pokeapi.co/api/v2/pokemon/";
+
+//Coge el CONJUNTO de TODOS los pokemons
+const getPokemons = (url) => {
+    fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data); //Muestra todos los pokemons con su url y nombre solo
+            showPokemons(data.results); //Muestra todos los pokemons pero con sus especificaciones
+        })
+}
+
+//Coge TODOS los pokemons pero por SEPARADO
+const showPokemons = (array) => {
+    array.map(item => { //filtramos el resultado
+        fetch(item.url) //Cogemos el enlace de cada pokemon
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            loadCard(data);
+        })
+    })
+}
+
+const loadCard = (data) => {
+    const imagen = data.sprites.other.home.front_default;
+    const name = data.name;
+
+    let card = document.createElement("div");
+    let content = `
+        <img src="${imagen}" alt="${name}" width="100px">
+        <p>${name}</p>
+        <p>${data.order}</p>
+    `;
+    card.innerHTML = content;
+    container.appendChild(card);
+}
+
+
+
+getPokemons(`${urlPokeApi}?offset=0&limit=151`)
+
+
