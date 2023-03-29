2fad3397bcb288fb63346fff3bba2bb9fcd633b5
Alejandro Palacios Martín <srjalean@gmail.com>
2023-03-29 11:42:37 +0200

    CAMBIOS
    he añadido la carpeta en img con los iconos de los tipos de los pokemons y tambien he actualizado el javascript para que aparezcan los tipos

--- javascriptGeneral.js
+++ javascriptGeneral.js
@@ -1,91 +1,39 @@
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-  .then((response) => response.json())
-  .then((data) => {
+  .then(response => response.json())
+  .then(data => {
     const pokemon = data.results;
     const container = document.querySelector(".grid-container");
     const searchInput = document.querySelector("#search-input");
     const updatePokemonList = () => {
       const searchTerm = searchInput.value.toLowerCase();
-
-      while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-
-      pokemon
-        .filter((p) => p.name.toLowerCase().includes(searchTerm))
-        .forEach((p) => {
-          
+      container.innerHTML = '';
+      pokemon.filter(p => p.name.toLowerCase().includes(searchTerm))
+        .forEach(p => {
           const card = document.createElement("div");
           card.classList.add("pokemon-card");
-
-          const name = document.createElement("span");
-          name.textContent = p.name;
-          name.style.marginTop = "10px";
-
-          const idSpan = document.createElement("span");
-          idSpan.textContent = `#${p.url.split("/")[6].padStart(3, "0")}`;
-          idSpan.style.fontWeight = "bold";
-          idSpan.style.display = "block";
-          idSpan.style.textAlign = "center";
-
-          const image = document.createElement("img");
-          
+          card.innerHTML = `
+            <span style="margin-top: 10px; text-align: center;">${p.name}</span>
+            <span style="font-weight: bold; display: block; text-align: center;">#${p.url.split("/")[6].padStart(3, "0")}</span>
+            <img src="">
+            <div class="types" style="display: flex; justify-content: center;"></div>
+          `;
+          card.style.cssText = "display: flex; flex-direction: column; justify-content: center; width: 225px; height: 325px; border: 1px solid #ddd; border-radius: 10px; background-color: #f1f1f1;";
           fetch(p.url)
-            .then((response) => response.json())
-            .then((data) => {
-              image.src = data.sprites.other.home.front_default;
+            .then(response => response.json())
+            .then(data => {
+              card.querySelector('img').src = data.sprites.other.home.front_default;
+              const typesContainer = card.querySelector('.types');
+              data.types.forEach(type => {
+                const typeElement = document.createElement('img');
+                typeElement.src = `./img/tiposPokemon/${type.type.name}.png`;
+                typeElement.style.cssText = "width: 30px; height: 30px; margin: 5px;";
+                typesContainer.appendChild(typeElement);
+              });
             })
-            .catch((error) => {
-              console.error(error);
-            });
-
-          card.appendChild(name);
-          card.appendChild(idSpan); 
-          card.appendChild(image);
-          
-          card.style.display = "flex";
-          card.style.flexDirection = "column";
-          card.style.justifyContent = "center";
-          card.style.width = "225px";
-          card.style.height = "275px";
-          card.style.border = "1px solid #ddd";
-          card.style.borderRadius = "10px";
-          card.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.7)";
-          card.style.textAlign = "center";
-          card.style.backgroundColor = "#c24e4e";
-          
+            .catch(error => console.error(error));
           container.appendChild(card);
         });
     };
-
     searchInput.addEventListener("input", updatePokemonList);
- 
     updatePokemonList();
-
-    pokemon.forEach((pokemon, index) => {
-      const card = document.createElement("div");
-      card.classList.add("pokemon-card");
-
-      const image = document.createElement("img");
-      const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
-      image.src = imageURL;
-      card.appendChild(image);
-
-      const name = document.createElement("span");
-      name.textContent = pokemon.name;
-
-      const idSpan = document.createElement("span");
-      idSpan.textContent = `#${index + 1}`; 
-      idSpan.style.fontWeight = "bold";
-      idSpan.style.display = "block";
-      idSpan.style.textAlign = "center";
-
-      card.appendChild(name);
-      card.appendChild(idSpan);
-
-      container.appendChild(card);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
   });
