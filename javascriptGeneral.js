210a960af219b22a067591a605e45e018ad61d89
Alejandro Palacios Martín <srjalean@gmail.com>
2023-04-03 22:38:48 +0200

    cambios varios
    he ordenado el css por secciones (con comentarios), he solucionado lo del modo oscuro/modo claro y he puesto bien los botones de github

--- javascriptGeneral.js
+++ javascriptGeneral.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myBtn").style.display = "block";
@@ -7,13 +6,9 @@ function scrollFunction() {
     document.getElementById("myBtn").style.display = "none";
   }
 }
-
-// Cuando se hace clic en el botón, se desplaza hacia arriba hasta la parte superior de la página
 function topFunction() {
-  document.body.scrollTop = 0; // Para Safari
-  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+  document.documentElement.scrollTop = 0;
 }
-
 
 
 const toggleModeButton = document.querySelector("#claroscuro");
@@ -22,11 +17,11 @@ const solImage = document.querySelector("#sol");
 
 
 toggleModeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark_mode");
+  document.querySelector(".pokemon_card").classList.toggle("dark_mode");
   toggleModeButton.classList.toggle('active');
 
-  // Actualizamos la imagen del botón
-  if (document.body.classList.contains("dark-mode")) {
+  if (document.body.classList.contains("dark_mode")) {
     lunaImage.style.display = "none";
     solImage.style.display = "block";
     localStorage.setItem('modeDark', 'true');
@@ -36,27 +31,22 @@ toggleModeButton.addEventListener("click", () => {
     localStorage.setItem('modeDark','false');
   }
 
-  //Obtener el modo actual
-if(localStorage.getItem('modeDark') === 'true'){ //Coge (get) la clave (modeDark) y comparamos si es igual al valor que marcamos arriba
-                                                 //en este caso true, podría ser otro, depend elo que pusimos arriba
-   document.body.classList.add('dark-mode');  //Metemos el efecto dark del archivo .css
-   modoOscuro.classList.add('active');   //Añadimos también que está activo
-}
+  if(localStorage.getItem('modeDark') === 'true'){
+    document.body.classList.add('dark_mode');
+    toggleModeButton.classList.add('active');
+  }
 else{
-    document.body.classList.remove('dark-mode'); //Borramos el efecto dark del archivo .css
-    modoOscuro.classList.remove('active');  //Borramos el efecto que indica que esta activo, para que se vea que no lo esta ya
+    document.body.classList.remove('dark_mode');
+    toggleModeButton.classList.remove('active');
 }
-
-  // Actualizamos el estilo de las tarjetas de Pokémon
-  const pokemonCards = document.querySelectorAll(".pokemon-card");
+ 
+  const pokemonCards = document.querySelectorAll(".pokemon_card");
   pokemonCards.forEach((card) => {
-    card.style.backgroundColor = document.body.classList.contains("dark-mode") ? "#666" : "#f1f1f1";
-    card.querySelector(".name").style.color = document.body.classList.contains("dark-mode") ? "#fff" : "#000";
-    card.querySelector("span:nth-of-type(2)").style.color = document.body.classList.contains("dark-mode") ? "#fff" : "#000";
+  card.style.backgroundColor = document.body.classList.contains("dark_mode") ? "#666" : "#f1f1f1";
+  card.querySelector(".name").style.color = document.body.classList.contains("dark_mode") ? "#fff" : "#000";
+ card.querySelector("span:nth-of-type(2)").style.color = document.body.classList.contains("dark_mode") ? "#fff" : "#000";
   });
 });
-
-
 
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151") 
@@ -66,44 +56,20 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     const container = document.querySelector(".grid-container");
     const searchInput = document.querySelector("#search-input");
     const typeColors = {
-      normal: '#A8A878',
-      fire: '#F08030',
-      water: '#6890F0',
-      electric: '#F8D030',
-      grass: '#78C850',
-      ice: '#98D8D8',
-      fighting: '#C03028',
-      poison: '#A040A0',
-      ground: '#E0C068',
-      flying: '#A890F0',
-      psychic: '#F85888',
-      bug: '#A8B820',
-      rock: '#B8A038',
-      ghost: '#705898',
-      dragon: '#7038F8',
-      dark: '#705848',
-      steel: '#B8B8D0',
-      fairy: '#EE99AC'
+      normal: '#A8A878',  fire: '#F08030',  water: '#6890F0',
+      electric: '#F8D030',  grass: '#78C850',  ice: '#98D8D8',
+      fighting: '#C03028',  poison: '#A040A0',  ground: '#E0C068',
+      flying: '#A890F0',  psychic: '#F85888',  bug: '#A8B820',
+      rock: '#B8A038',  ghost: '#705898',  dragon: '#7038F8',
+      dark: '#705848',  steel: '#B8B8D0',  fairy: '#EE99AC'
     };
     const typeTranslations = {
-      normal: 'normal',
-      fire: 'fuego',
-      water: 'agua',
-      electric: 'eléctrico',
-      grass: 'planta',
-      ice: 'hielo',
-      fighting: 'lucha',
-      poison: 'veneno',
-      ground: 'tierra',
-      flying: 'volador',
-      psychic: 'psíquico',
-      bug: 'bicho',
-      rock: 'roca',
-      ghost: 'fantasma',
-      dragon: 'dragón',
-      dark: 'siniestro',
-      steel: 'acero',
-      fairy: 'hada'
+      normal: 'normal',  fire: 'fuego',  water: 'agua',
+      electric: 'eléctrico',  grass: 'planta',  ice: 'hielo',  
+      fighting: 'lucha',  poison: 'veneno',  ground: 'tierra',
+      flying: 'volador',  psychic: 'psíquico',  bug: 'bicho',  
+      rock: 'roca',  ghost: 'fantasma',  dragon: 'dragón',
+      dark: 'siniestro',  steel: 'acero',  fairy: 'hada'
     };
     const updatePokemonList = () => {
       const searchTerm = searchInput.value.toLowerCase();
@@ -111,14 +77,12 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
       pokemon.filter(p => p.name.toLowerCase().includes(searchTerm))
         .forEach(p => {
           const card = document.createElement("div");
-          card.classList.add("pokemon-card");
-          card.classList.add("mover");
+          card.classList.add("pokemon_card");
           card.innerHTML = `
-            <span class="name" style="margin-top: 10px; text-align:center; font-size: 18px;">${p.name.charAt(0).toUpperCase() + p.name.slice(1)}</span>
-            <span style="font-weight:bold; display:block; text-align:center;">#${p.url.split("/")[6].padStart(3, "0")}</span>
-            <img src="" class="mover">
-            <div class="types" style="display:flex; justify-content:center;"></div>`;
-          card.style.cssText = "cursor: pointer; display:flex; flex-direction:column; justify-content:center; width:250px; height:350px; border:1px solid #ddd; border-radius:10px; background-color:#f1f1f1;";
+            <span class="name">${p.name.charAt(0).toUpperCase() + p.name.slice(1)}</span>
+            <span class="numero">#${p.url.split("/")[6].padStart(3, "0")}</span>
+            <img src="">
+            <div class="types"></div>`;
           
           card.addEventListener('click', () => { window.location.href = `especifico.html?id=${p.url.split("/")[6]}`; });
                  
