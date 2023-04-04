6e0d1ebc84c55213ebf9fdafffdda51db108ee91
Alejandro Palacios Martín <srjalean@gmail.com>
2023-04-04 18:36:54 +0200

    limpieza
    limpieza del codigo javascript

--- javascriptGeneral.js
+++ javascriptGeneral.js
@@ -89,97 +89,23 @@ else{
                typeElement.textContent = typeTranslations[typeName];
                typeElement.style.cssText = `background-color:${typeColors[typeName]}; color:white; padding:5px 10px; border-radius:10px; margin-right:5px; text-shadow:-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; border:solid black 2px`;
                typesContainer.appendChild(typeElement);
-            });
-                  const descriptionButton = card.querySelector(".description-button img"
-                      
-                    );
-                    const speciesUrl = data.species.url;
-                    fetch(speciesUrl)
-                      .then((response) => response.json())
-                      .then((speciesData) => {
-                        const flavorTextEntries =
-                          speciesData.flavor_text_entries;
-                        const englishEntries = flavorTextEntries.filter(
-                          (entry) => entry.language.name === "en"
-                        );
-                        const firstEntry = englishEntries[0];
-                        descriptionButton.title = firstEntry.flavor_text;
-                      })
+              });
+                const descriptionButton = card.querySelector(".description-button img");
+                const speciesUrl = data.species.url;
+                fetch(speciesUrl)
+                  .then((response) => response.json())
+                  .then((speciesData) => {
+                    const flavorTextEntries =
+                      speciesData.flavor_text_entries;
+                    const englishEntries = flavorTextEntries.filter(
+                      (entry) => entry.language.name === "en");
+                    const firstEntry = englishEntries[0];
+                    descriptionButton.title = firstEntry.flavor_text;
+                  })
            })
-            .catch(error => console.error(error));
-        container.appendChild(card);
+          container.appendChild(card);
        });
-   };
+    };
    searchInput.addEventListener("input", updatePokemonList);
    updatePokemonList();
  });
-
-// fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-//   .then(response => response.json())
-//   .then(data => {
-//     const pokemon = data.results;
-//     const container = document.querySelector(".grid-container");
-//     const searchInput = document.querySelector("#search-input");
-
-
-//     const typeColors = {
-//         normal: '#A8A878',  fire: '#F08030',  water: '#6890F0',
-//            electric: '#F8D030',  grass: '#78C850',  ice: '#98D8D8',
-//            fighting: '#C03028',  poison: '#A040A0',  ground: '#E0C068',
-//          flying: '#A890F0',  psychic: '#F85888',  bug: '#A8B820',
-//       rock: '#B8A038',  ghost: '#705898',  dragon: '#7038F8',
-//          dark: '#705848',  steel: '#B8B8D0',  fairy: '#EE99AC'
-//        };
-//        const typeTranslations = {
-//          normal: 'normal',  fire: 'fuego',  water: 'agua',
-//         electric: 'eléctrico',  grass: 'planta',  ice: 'hielo',  
-//           fighting: 'lucha',  poison: 'veneno',  ground: 'tierra',
-//          flying: 'volador',  psychic: 'psíquico',  bug: 'bicho',  
-//          rock: 'roca',  ghost: 'fantasma',  dragon: 'dragón',
-//          dark: 'siniestro',  steel: 'acero',  fairy: 'hada'
-//       };
-//     const updatePokemonList = () => {
-//       const searchTerm = searchInput.value.toLowerCase();
-//       container.innerHTML = '';
-//       pokemon.filter(p => p.name.toLowerCase().includes(searchTerm))
-//         .forEach(p => {
-//           const card = document.createElement("div");
-//           card.classList.add("pokemon_card");
-//           card.innerHTML = `
-//             <span class="name">${p.name.charAt(0).toUpperCase() + p.name.slice(1)}</span>
-//             <span class="numero">#${p.url.split("/")[6].padStart(3, "0")}</span>
-//             <img src="">
-//             <button class="description-button"><img src="./img/info.png"></button>`;
-
-//           card.addEventListener('click', () => { window.location.href = `especifico.html?id=${p.url.split("/")[6]}`; });
-
-//           fetch(p.url)
-//                   .then((response) => response.json())
-//                   .then((data) => {
-//                     card.querySelector("img").src =
-//                       data.sprites.other.home.front_default;
-
-//                     const descriptionButton = card.querySelector(".description-button img"
-                      
-//                     );
-//                     const speciesUrl = data.species.url;
-//                     fetch(speciesUrl)
-//                       .then((response) => response.json())
-//                       .then((speciesData) => {
-//                         const flavorTextEntries =
-//                           speciesData.flavor_text_entries;
-//                         const englishEntries = flavorTextEntries.filter(
-//                           (entry) => entry.language.name === "en"
-//                         );
-//                         const firstEntry = englishEntries[0];
-//                         descriptionButton.title = firstEntry.flavor_text;
-//                       })
-                      
-//                   })
-//                   .catch((error) => console.error(error));
-//                 container.appendChild(card);
-//               });
-//           };
-//     searchInput.addEventListener("input", updatePokemonList);
-//     updatePokemonList();
-//   });
